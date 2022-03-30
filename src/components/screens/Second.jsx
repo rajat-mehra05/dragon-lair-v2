fefe29867fbef039208fa925db0e955d78e1d358
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-03-30 21:59:51 +0530

    fixed second screen animation

--- src/components/screens/Second.jsx
+++ src/components/screens/Second.jsx
@@ -2,92 +2,99 @@ import { AnimatePresence, motion, useAnimation } from "framer-motion";
 import { useEffect } from "react";
 import mountain from "../../assets/images/mountains.png";
 
+const mountVariants = {
+  initial: {
+    y: "80%",
+    opacity: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+
+  animate: {
+    y: "50%",
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: 2,
+    },
+  },
+
+  exit: {
+    scale: 1.5,
+    y: "-20%",
+    opacity: 0,
+    transition: {
+      duration: 2,
+    },
+  },
+};
+
+const textVariants = {
+  initial: {
+    opacity: 0,
+    top: "100%",
+  },
+  animate: (isSecond) => ({
+    top: isSecond ? "10%" : "19%",
+    opacity: 1,
+    transition: {
+      delay: isSecond ? 0.2 : 1.5,
+      duration: 2,
+    },
+  }),
+  exit: {
+    top: "100%",
+    opacity: 0,
+    transition: {
+      duration: 2,
+    },
+  },
+};
+
 const Second = ({ currentView }) => {
   const mountainControls = useAnimation();
 
   useEffect(() => {
-    if (currentView === 2) {
-      mountainControls.start({
-        y: "50%",
-        scale: 1,
-        opacity: 1,
-        transition: {
-          duration: 1,
-          delay: 2,
-        },
-      });
+    if (currentView === 2) mountainControls.start("animate");
 
-      if (currentView === 1) {
-        mountainControls.start({
-          y: "80%",
-          opacity: 0,
-        });
-      }
-    }
+    if (currentView === 1) mountainControls.start("initial");
+    if (currentView === 4) mountainControls.start("exit");
   }, [currentView, mountainControls]);
 
   return (
     <div className="flex h-full flex-col absolute z-[2] w-full justify-center items-center">
-      <AnimatePresence exitBeforeEnter>
-        {(currentView === 2 || currentView === 3) && (
-          <motion.div
-            className="w-full absolute bottom-0 left-0 z-[3]"
-            initial={{
-              y: "80%",
-              opacity: 0,
-            }}
-            animate={mountainControls}
-            exit={{
-              scale: 1.5,
-              y: "-20%",
-              opacity: 0,
-              transition: {
-                duration: 2,
-              },
-            }}
-          >
-            <img src={mountain} alt="mountain" />
-          </motion.div>
-        )}
-      </AnimatePresence>
+      <motion.div
+        className="w-full absolute bottom-0 left-0 z-[3]"
+        variants={mountVariants}
+        initial="initial"
+        animate={mountainControls}
+      >
+        <img src={mountain} alt="mountain" />
+      </motion.div>
 
       <AnimatePresence exitBeforeEnter>
         {currentView === 2 && (
           <motion.div
-            initial={{
-              opacity: 0,
-              y: "200%",
-            }}
-            animate={{
-              opacity: 1,
-              y: -60,
-              transition: {
-                delay: 2,
-                duration: 2,
-              },
-            }}
-            exit={{
-              opacity: 0,
-              y: 200,
-              transition: {
-                duration: 2,
-              },
-            }}
-            className="w-full flex justify-around px-2"
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            custom={false}
+            className="w-full absolute left-0 flex justify-around px-2"
           >
-            <p className="text-white w-[35%] pl-20">
+            <p className="text-white w-[35%] pl-20 ">
               <h2 className="text-[#e28529] pb-8 text-2xl mt-8">
-                {" "}
-                Who are we ?{" "}
+                Who are we ?
               </h2>
               Dragon's Lair is a launchpad for the most daring and innovate of
               new crypto and NFT projects.
             </p>
             <p className="text-white w-[28%] pb-12">
               <h2 className="text-[#e28529] pb-8 text-2xl">
-                {" "}
-                What's our goal ?{" "}
-              </h2>{" "}
+                What's our goal ?
+              </h2>
               The crypto Dragonland is full of <br /> rugpulls and scammers,{" "}
               <br /> fat private sales <br /> and inexperienced teams. <br />{" "}
               <br /> Our dragons will incinerate these projects and only the
@@ -100,26 +107,12 @@ const Second = ({ currentView }) => {
       <AnimatePresence exitBeforeEnter>
         {currentView === 3 && (
           <motion.div
-            initial={{
-              opacity: 0,
-              y: "200%",
-            }}
-            animate={{
-              opacity: 1,
-              y: -100,
-              transition: {
-                delay: 1,
-                duration: 2,
-              },
-            }}
-            exit={{
-              opacity: 0,
-              y: 200,
-              transition: {
-                duration: 2,
-              },
-            }}
-            className="w-full flex justify-around"
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            custom={true}
+            className="w-full absolute left-0 flex justify-around"
           >
             <div className="w-[30%] pl-20 py-4 mt-16">
               <h2 className="text-[#e28529] pb-8 text-2xl">
